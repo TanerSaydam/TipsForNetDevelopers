fd40e341a8fbb0cbf6654c5045a99bf7ecd6c9ea
tanersaydam <tanersaydam@gmail.com>
2023-04-21 20:26:28 +0300

    CancellationToken

---
+++ CancellationTokenWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CancellationTokenWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+    {
+        //İşlemler
+
+
+
+        AppDbContext context = new();
+
+        IList<Product> products = await context.Products.ToListAsync(cancellationToken);
+        return Ok(products); 
+    }
+}
