a591ee25a91b67bd090e656242a95efc84a09b8a
tanersaydam <tanersaydam@gmail.com>
2023-04-21 16:38:12 +0300

    AsNoTracking

---
+++ AsNoTracking/BencmarkService.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsNoTracking;
+
+[ShortRunJob, Config(typeof(Config))]
+public class BencmarkService
+{
+    private class Config : ManualConfig
+    {
+        public Config()
+        {
+            SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithRatioStyle(BenchmarkDotNet.Columns.RatioStyle.Trend);
+        }
+    }
+
+    //[Benchmark(Baseline = true)]
+    //public void GetAllWithTracking()
+    //{
+    //    AppDbContext context = new();
+
+    //    context.Products.ToList();
+    //}
+
+    //[Benchmark]
+    //public void GetAllWithAsNoTracking()
+    //{
+    //    AppDbContext context = new();
+
+    //    context.Products.AsNoTracking().ToList();
+    //}
+
+    //[Benchmark(Baseline = true)]
+    //public void GetFirstWithTracking()
+    //{
+    //    AppDbContext context = new();
+
+    //    context.Products.First();
+    //}
+
+    //[Benchmark]
+    //public void GetFirstWithAsNoTracking()
+    //{
+    //    AppDbContext context = new();
+
+    //    context.Products.AsNoTracking().First();
+    //}
+
+    public void Find()
+    {
+        AppDbContext context = new();
+
+        context.Products.FromSqlRaw("Select * From").AsNoTracking().ToList();
+    }
+}
---
+++ AsNoTracking/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace AsNoTracking;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        BenchmarkRunner.Run<BencmarkService>();
+    }
+}
