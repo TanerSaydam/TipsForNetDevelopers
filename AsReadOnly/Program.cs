a056a2f62703c297a2bc04a60a0855da40fc7d88
tanersaydam <tanersaydam@gmail.com>
2023-04-22 12:25:44 +0300

    AsReadOnly

---
+++ AsReadOnly/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace AsReadOnly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new();
+            IReadOnlyList<Product> products = context.Products.ToList().AsReadOnly();
+
+            products[0].Name = "Deneme 2";
+
+            Console.WriteLine(products[0].Name);
+        }
+    }
+}
