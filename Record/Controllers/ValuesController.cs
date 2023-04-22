1686cd109a804187ea1eebd234b63996e43d3b05
tanersaydam <tanersaydam@gmail.com>
2023-04-22 10:29:16 +0300

    Record

---
+++ Record/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Record.Dtos;
+
+namespace Record.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            //LoginDto loginDto2 = new()
+            //{
+            //    Email = "tanersaydam@gmail.com",
+            //    Password = "1",
+            //    RememberMe = true
+            //};
+            //loginDto2.Email = string.Empty;
+
+            return Ok("Giriş başarılı");
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult LoginWithRecord(Login login)
+        {
+            //Login login2 = new("tanersaydam@gmail.com", "1");            
+            return Ok("Giriş başarılı");
+        }
+    }
+}
---
+++ Record/Dtos/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Record.Dtos
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public record Login(
+        string Email,
+        string Password,
+        bool RememberMe = true);
+}
