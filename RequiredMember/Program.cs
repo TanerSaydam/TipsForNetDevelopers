cefd61e1713b87e17a33007649049a2def165b83
tanersaydam <tanersaydam@gmail.com>
2023-04-29 20:49:58 +0300

    Required Member

---
+++ RequiredMember/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RequiredMember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new() { Name ="Deneme"};
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Product
+    {        
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
